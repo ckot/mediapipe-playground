e812b6edd0feb84747cbb0458b2e825285f91934
ckot <scott.t.silliman@gmail.com>
2024-08-31 00:26:49 -0400

    re-added debugging

--- src/mediapipe_playground/utils.py
+++ src/mediapipe_playground/utils.py
@@ -93,16 +93,17 @@ class Segmenter:
         ret_val = {}
         mp_img = mp.Image(image_format=mp.ImageFormat.SRGB, data=resized_image)
         segmentation_result = self._segmenter.segment(mp_img)
-        categories = segmentation_result.category_mask[0].numpy_view() # img_size x img_size of 0 - num classes
-        # landmarks_mask = np.zeros(categories.shape, dtype=np.uint8)
-        # detection_result = self._detector.detect(resized_image)
-        # grab landmarks for 1st face only
-        # face_landmarks = detection_result.face_landmarks[0]
-        # print("face_landmarks", dir(face_landmarks))
-        # convert categories img to N images of same shape of 0 or 1
-        # mapped to their category name
-        for cat_id, mask_name in enumerate(self._masks):
-            ret_val[mask_name] = (categories == cat_id).astype(int)
+        print(segmentation_result)
+        # categories = segmentation_result.category_mask[0].numpy_view() # img_size x img_size of 0 - num classes
+        # # landmarks_mask = np.zeros(categories.shape, dtype=np.uint8)
+        # # detection_result = self._detector.detect(resized_image)
+        # # grab landmarks for 1st face only
+        # # face_landmarks = detection_result.face_landmarks[0]
+        # # print("face_landmarks", dir(face_landmarks))
+        # # convert categories img to N images of same shape of 0 or 1
+        # # mapped to their category name
+        # for cat_id, mask_name in enumerate(self._masks):
+        #     ret_val[mask_name] = (categories == cat_id).astype(int)
         return ret_val
 
     def __ensure_model_downloaded(self, model_name):
