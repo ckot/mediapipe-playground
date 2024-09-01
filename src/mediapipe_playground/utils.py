8163f0bc037eacb0ef645581b4d1170c0903db92
ckot <scott.t.silliman@gmail.com>
2024-09-01 01:19:01 -0400

    added more debug info

--- src/mediapipe_playground/utils.py
+++ src/mediapipe_playground/utils.py
@@ -98,6 +98,7 @@ class Segmenter:
         detection_result = self._detector.detect(mp_img)
         # landmarks are normalized (0-1) 3d ignore z coord and
         # multiply x * width and y * height
+        print(type(detection_result.face_landmarks))
         for i, lm in enumerate(detection_result.face_landmarks, start=1):
             print(i, lm)
             x = lm.x * self._img_size[0]
